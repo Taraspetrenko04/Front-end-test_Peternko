3ded86f78f5e7120ccb16b172e3096c26edc9b28
Taras PETRENKO <Taraspetrenko04@gmail.com>
2019-03-11 19:57:30 +0200

    Make a checking for a prime number

--- Task_6/script.js
+++ Task_6/script.js
@@ -1,11 +1,32 @@
- 
-let num = 40;
-let arrPrime = [];
-    for ( let i = num; i <= num; i--){
-         for (let j = 2; j>=num; j++) {
-             if(i % j != 0 ){
-                arrPrime.push(i);
-          } 
-        }
-        console.log(arrPrime);
+let num = parseInt(prompt("Enter a number", ""));
+console.log(typeof(num));
+
+// НАПИСАТЬ ПРОГРАМУ НА ПРОВЕРКУ ПРОСТОГО ЧИСЛА
+    function isPrime (x){
+        let flag = false;
+        for(let k = 1, flag = false ; k<x; k++ flag = 0){
+            if(x % k == 0 ){
+                flag = true;
+                return console.log(findPrimes(num)) ;
+            }  return alert("Вы ввели простое число");
+                break;
     }
+}
+
+function findPrimes (x){
+    let arrPrime = [];
+    for(let i = 1, flag = false; i <= x; i++, flag = 0){
+        for(let j = 2; j * 2 <= i; j++){
+            if(i % j == 0){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            arrPrime.unshift(i);
+    }
+    return arrPrime;
+    console.log(arrPrime);
+}
+// console.log(findPrimes(num));
+console.log(isPrime(num));
