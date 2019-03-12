016cc4697cece839adfb7cadaa7708c7258e236a
Taras PETRENKO <Taraspetrenko04@gmail.com>
2019-03-12 18:20:30 +0200

    2/3 of task 6 has done

--- Task_6/script.js
+++ Task_6/script.js
@@ -1,32 +1,34 @@
-let num = parseInt(prompt("Enter a number", ""));
+let num = parseInt(prompt("Enter a positive number, please", ""));
 console.log(typeof(num));
-
-// НАПИСАТЬ ПРОГРАМУ НА ПРОВЕРКУ ПРОСТОГО ЧИСЛА
-    function isPrime (x){
-        let flag = false;
-        for(let k = 1, flag = false ; k<x; k++ flag = 0){
-            if(x % k == 0 ){
-                flag = true;
-                return console.log(findPrimes(num)) ;
-            }  return alert("Вы ввели простое число");
-                break;
-    }
-}
-
-function findPrimes (x){
-    let arrPrime = [];
-    for(let i = 1, flag = false; i <= x; i++, flag = 0){
-        for(let j = 2; j * 2 <= i; j++){
-            if(i % j == 0){
-                flag = true;
-                break;
-            }
-        }
-        if(!flag)
-            arrPrime.unshift(i);
-    }
-    return arrPrime;
-    console.log(arrPrime);
-}
-// console.log(findPrimes(num));
-console.log(isPrime(num));
+ //Написать блок гкоторый будет делить число на все елементы массыва
+// if (num <=3 ){
+//         alert("Number is prime or negative, please enter again");
+//     }else{
+//         let k=Math.round(Math.sqrt(num));
+//         let flag = false; 
+//             for(i=2; i<k+1; i++)
+//                 if(num %i==0)
+//                 {
+//                 flag = true;
+//                 findPrimes(num);
+//                 break;
+//                 }
+                    
+//             if (flag == false)
+//             alert("Number is prime, please enter again");
+//             }
+// //___ Searching prime numbers from 1 to given number______
+// function findPrimes (x){
+//     let arrPrime = [];
+//     for(let i = 1, flag = false; i <= x; i++, flag = 0){
+//         for(let j = 2; j * 2 <= i; j++){
+//             if(i % j == 0){
+//                 flag = true;
+//                 break;
+//             }
+//         }
+//         if(!flag)
+//             arrPrime.unshift(i);
+//     } return console.log(arrPrime);// return arrPrime;  
+// }  
+// // __________________________________________________________
